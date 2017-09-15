2a708b252130c1b92ba251bcb8a931fa1387a8d2
aravinda-arkaje <aravindaarkaje@gmail.com>
2017-09-15 15:31:22 +0530

    Update Register_model.php

--- Register_model.php
+++ Register_model.php
@@ -2,8 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Register_model extends CI_model {
-
-
+	
+	//this function is for registering a user into database by checking the email enterd is already prasent or not
 	function process($data)
 	{
 		$this->db->where('email',$data['email']);
@@ -23,6 +23,7 @@ class Register_model extends CI_model {
 		}		
 	}
 	
+	//this function is to get the information fromthe database for the user to login checking after the registration
 	function get_details($data)
 	{
 		$this->db->where('email',$data['username']);
